f28492d8ff510aa2c8db085394b08853f38afbc3
OSFAV7 <72432488+OSFAV7@users.noreply.github.com>
2020-10-06 16:55:21 -0500

    cambios para template

--- PGprincipal/views.py
+++ PGprincipal/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import Template, context
-from django.template import loader
+from django.shortcuts import render, HttpResponse
+
 # Create your views here.
 
 
 def inicio(request):
-    plantilla= loader.get_template('ecommerce.html')
-    renderizar= plantilla.render()
-    return HttpResponse(renderizar)
+
+    return render(request, "PGprincipal/home.html")
