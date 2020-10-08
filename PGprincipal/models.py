11d7497382e7941d6dc987decee7f171180c868a
OSFAV7 <72432488+OSFAV7@users.noreply.github.com>
2020-10-08 11:31:41 -0500

    vistas por funciones

--- PGprincipal/models.py
+++ PGprincipal/models.py
@@ -2,9 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class Usuarios(models.Model):
-    nombre=models.CharField(max_length=30)
-    cargo=models.CharField(max_length=30)
-    email=models.EmailField()
-    telefono=models.CharField(max_length=10)
-    password=models.CharField(max_length=20)
