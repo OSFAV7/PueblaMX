f7ec77101d59b60cf6e8b6429a8d31b46b98a059
OSFAV7 <72432488+OSFAV7@users.noreply.github.com>
2020-10-09 00:22:18 -0500

    relaciones de asistencia-trabajador

--- ControlAsistencia/admin.py
+++ ControlAsistencia/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Empleados, asistencias
 
 # Register your models here.
+
+admin.site.register(Empleados)
+admin.site.register(asistencias)
--- ControlAsistencia/models.py
+++ ControlAsistencia/models.py
@@ -2,8 +2,28 @@ from django.db import models
 
 # Create your models here.
 class Empleados(models.Model):
-    nosocial=models.CharField(unique= True, max_length=30)
-    nombre=models.CharField(max_length=30)
-    apellidos=models.CharField(max_length=40)
-    cargo=models.CharField(max_length=30)
-    telefono=models.CharField(max_length=10)
+    nosocial=models.CharField('Numero social',primary_key= True, unique= True, max_length=30)
+    nombre=models.CharField('Nombres del empleado',max_length=30)
+    apellidos=models.CharField('Apellidos del empleado',max_length=40)
+    cargo=models.CharField('Cargo que desempeña',max_length=30)
+    telefono=models.CharField('Telefono de contacto',max_length=10)
+    asistencia=models.BooleanField('Asistencia del dia', default= False)
+
+    class Meta:
+        verbose_name= 'Empleado'
+        verbose_name_plural= 'Empleados'
+    
+    def __str__(self):
+        return self.nombre
+        
+class asistencias (models.Model):
+    empleado= models.ForeignKey(Empleados, on_delete=models.CASCADE)
+    fecha= models.DateField('Fecha de asistencia')
+
+    class Meta():
+        verbose_name= 'Asitencia'
+        verbose_name_plural= 'Asistencias'
+    
+    def __str__(self):
+        return '{} {}'.format(self.empleado, self.fecha)
+
