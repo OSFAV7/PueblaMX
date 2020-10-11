afc4f1683c4954ca3a6d29c99cbc3d709e951887
OSFAV7 <72432488+OSFAV7@users.noreply.github.com>
2020-10-11 01:07:38 -0500

    avance login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
 
 
-class login(TemplateView):
 
+class login(LoginView):
     template_name="usuarios/login.html"
+
